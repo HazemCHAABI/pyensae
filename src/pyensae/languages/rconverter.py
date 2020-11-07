268f4dc1eeccc8fe2a5296d0c160b473c7449a2f
HazemCHAABI <65767801+HazemCHAABI@users.noreply.github.com>
2020-11-07 10:58:46 +0100

    Update rconverter.py

--- src/pyensae/languages/rconverter.py
+++ src/pyensae/languages/rconverter.py
@@ -32,13 +32,8 @@ def r2python(code: str, pep8=False, fLOG=None) -> str:
             :showcode:
 
             rscript = '''
-                nb=function(y=1930){
-                debut=1816
-                MatDFemale=matrix(D$Female,nrow=111)
-                colnames(MatDFemale)=(debut+0):198
-                cly=(y-debut+1):111
-                deces=diag(MatDFemale[:,cly[cly%in%1:199]])
-                return(c(B$Female[B$Year==y],deces))}
+               hour_test_bin = ifelse(hour %in% most_freq_hours,1,
+                       ifelse(hour %in% least_freq_hours,2,3)),
                 '''
 
             from pyensae.languages import r2python
